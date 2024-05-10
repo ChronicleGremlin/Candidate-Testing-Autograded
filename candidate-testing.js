ee3aa9495c6be4e86bd4c5c04e657b893f2c983e
Alec <alecjost7@gmail.com>
2024-05-09 19:02:46 -0500

    Candidate Testing Part one pass npm test not runing question like previous commit

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,9 +1,9 @@
 const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
-let gradeQuiz = ' ';
-let askForName = ' ';
-let askQuestion = ' ';
+//let gradeQuiz = ' ';
+//let askForName = ' ';
+//let askQuestion = ' ';
 // TODO 1.1a: Define candidateName // 
 let candidateName = ' ';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
@@ -17,32 +17,33 @@ let questions;
 let correctAnswers;
 let candidateAnswers;
 
+function askForName(){
 
-if (askForName = ' ') {
   // TODO 1.1b: Ask for candidate's name //
 candidateName = input.question("What is your name?");
 }
-
-if (askQuestion = ' ') {
+function askQuestion(){
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   candidateAnswer = input.question(question);
 
 
 }
 
-if (candidateAnswer == correctAnswer) {
+
+function gradeQuiz(candidateAnswer){
+  if (candidateAnswer == correctAnswer) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  console.log('Correct!');
+    console.log('Correct!');
 
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
+    let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
-  return grade;
+    return grade;
   
 
 } else {
   console.log('Incorrect!');
-}
+}}
 
 function runProgram() {
   askForName();

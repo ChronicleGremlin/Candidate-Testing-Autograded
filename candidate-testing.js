6a3ac4c21f4815680761d4a8b3ea1d9079546744
Alec <alecjost7@gmail.com>
2024-05-22 15:55:55 -0500

    Candidate Testing Complete Questions arrays and loop, part II

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,8 +12,8 @@ let correctAnswer = "Sally Ride";
 let candidateAnswer = " ";
 
 //TODO: Variables for Part 2
-let questions = ["Who was the first American woman in space? ", "True or False: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"];
-let correctAnswers = ["Sally Ride", "True", "40", "Trajectory", "3"];
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 
 function askForName(){
@@ -21,9 +21,12 @@ function askForName(){
   candidateName = input.question("What is your name? ");
 }
 
-function askQuestion(){
+function askQuestions(){
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer = input.question(question);
+  for (let index = 0; index < 5; index++){
+    candidateAnswers.push(input.question(questions[index]));
+  }
+  
 }
 
 function gradeQuiz(answer){
@@ -43,9 +46,9 @@ function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
   console.log("Hello, " + candidateName + "! Please get ready for the quiz.");
-  askQuestion();
-  console.log(candidateAnswer);
-  gradeQuiz(candidateAnswer);
+  askQuestions();
+  console.log(`Correct answers are:  ${correctAnswers} Candidate answers are:  ${candidateAnswers}`);
+  gradeQuiz();
 }
 
 // ----------- Don't write any code or change any code below this line ---------- //
